e7018d5285d3ce5b1a1ab429ed6b297fbd2b90d6
tao1874 <hi1874@oulook.com>
2020-02-22 20:40:24 +0800

    change baseurl

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: '挖煤工', // Title for the site. This will be displayed in the navbar.
     theme: '@vuepress/theme-blog',
+    base:"/blog/",
     themeConfig: {
       // Please keep looking down to see the available options.
     }
