5369ac77ca1a5700cd6ea7f7505d76c1dba13f08
tao1874 <hi1874@oulook.com>
2020-02-23 11:06:43 +0800

    update pwa theme color

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     head: [
       ['link', { rel: 'icon', href: '/favicon.ico' }],
       ['link', { rel: 'manifest', href: '/manifest.json' }],
-      ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+      ['meta', { name: 'theme-color', content: '#d05dd2' }],
       ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
       ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
       ['link', { rel: 'apple-touch-icon', href: '/icons/apple-icon-152x152.png' }],
